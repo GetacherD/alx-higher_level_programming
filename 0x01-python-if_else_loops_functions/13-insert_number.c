02baa3c2c020066b8b4951b5843c2727dccbc2d1
Getacher <Getacher.d@gmail.com>
2022-05-31 19:12:16 +0300

    final

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,11 +1,18 @@
 #include "lists.h"
-
+/**
+* insert_node - insert node in sorted list
+* @head: head pointer
+* @number: new value for the node
+* Return: head pointer
+*/
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *Node, *new;
+
+	Node = *head;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-		return (*head);
+		return (NULL);
 	new->n = number;
 	new->next = NULL;
 	if (head == NULL || *head == NULL)
@@ -13,29 +20,27 @@ listint_t *insert_node(listint_t **head, int number)
 		*head = new;
 		return (*head);
 	}
-
-	if (number < (*head)->n)
+	while (Node)
 	{
-		new->next= *head;
-		*head = new;
-		return (*head);
-	}
-	Node = *head;
-	while(Node)
-	{
-		if(number >= Node->n && number <= Node->next->n)
+		if (Node->n >= number)
+		{
+			new->next = Node;
+			*head = new;
+			return (*head);
+		}
+		if (Node->next && (Node->next->n >= number))
 		{
 			new->next = Node->next;
 			Node->next = new;
 			return (*head);
 		}
-		Node = Node->next;
 		if (Node->next == NULL)
 		{
 			Node->next = new;
+			new->next = NULL;
 			return (*head);
 		}
-
+		Node = Node->next;
 	}
 	return (*head);
 }
