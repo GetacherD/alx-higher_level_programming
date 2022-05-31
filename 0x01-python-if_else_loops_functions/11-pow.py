d53787818931339833b5dd5ee43242d7af3bb6a2
Getacher <Getacher.d@gmail.com>
2022-05-31 13:55:12 +0300

    use buitin

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    tmp = 1/a
-    tmp2 = a
-    if b == 0:
-        return 1
-    elif b < 0:
-        for i in range(1, -b):
-            tmp = tmp * (1/a)
-        a = tmp
-    else:
-        for i in range(1, b):
-            a = tmp2*a
-    return a
+    return a ** b
