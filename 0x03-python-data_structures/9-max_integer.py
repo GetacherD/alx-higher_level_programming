bbbcc658ed3972a0e9d462259359c40b5248cf2d
Getacher <Getacher.d@gmail.com>
2022-06-05 08:33:31 +0300

    fix min to first element

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,9 +3,9 @@
 
 def max_integer(my_list=[]):
 
-    maximum = 0
     if (my_list is None) or (len(my_list) == 0):
         return None
+    maximum = my_list[0]
     for i in my_list:
         if i > maximum:
             maximum = i
