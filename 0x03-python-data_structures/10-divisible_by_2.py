6e2b53916c3943458be3cfb333f17ef1e10960c3
Getacher <Getacher.d@gmail.com>
2022-06-05 08:41:54 +0300

    fix typo

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,7 +5,7 @@ def divisible_by_2(my_list=[]):
 
     if my_list is None or len(my_list) == 0:
         return None
-    multple_of_2 = []
+    multiple_of_2 = []
     for i in my_list:
         if i % 2 == 0:
             multiple_of_2.append(True)
