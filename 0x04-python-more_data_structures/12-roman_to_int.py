6b854cd5dc95bf5286a0209ea5b355d31e407c9c
Getacher <Getacher.d@gmail.com>
2022-06-08 14:32:32 +0300

    first

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+
+def roman_to_int(roman_string):
+
+    digits = ["I", "V", "X", "L", "C", "D", "M"]
+    val = [1, 5, 10, 50, 100, 500, 1000]
+    sym = list(roman_string)
+    tot = 0
+    for c in sym:
+        tot = tot + val[digits.index(c)]
+    if "M" in sym and "C" in sym and sym.index("M") > sym.index("C"):
+        tot = tot - 200
+    if "C" in sym and "X" in sym and sym.index("C") > sym.index("X"):
+        tot = tot - 20
+    if "X" in sym and "L" in sym and sym.index("L") > sym.index("X"):
+        tot = tot - 20
+    if "V" in sym and "I" in sym and sym.index("V") > sym.index("I"):
+        tot = tot - 2
+    if "X" in sym and "I" in sym and sym.index("X") > sym.index("I"):
+        tot = tot - 2
+    if "X" in sym and "C" in sym and sym.index("C") > sym.index("X"):
+        tot = tot - 20
+    if "C" in sym and "D" in sym and sym.index("D") > sym.index("C"):
+        tot = tot - 200
+    return tot
