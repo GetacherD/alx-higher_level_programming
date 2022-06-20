5a0bc2c05d0f16cc5189820356e692e4737f700e
Getacher <Getacher.d@gmail.com>
2022-06-20 13:09:31 +0300

    test

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -5,11 +5,12 @@ def magic_calculation(a, b):
 
     result = 0
     for i in range(1, 3):
-        if not (i > a):
-            raise Exception("Too far")
         try:
-            result += (a ** b) / i
-        except:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / i
+        except Exception:
             result = a + b
-            continue
+            break
     return result
