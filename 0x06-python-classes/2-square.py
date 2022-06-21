5673eb6457b5ba0558d42113f386320fca29f6d7
Getacher <Getacher.d@gmail.com>
2022-06-21 20:58:33 +0300

    add doc

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+""" Define Square Class ."""
 
 
 class Square:
 
+    """ Represent Squre Class. """
     def __init__(self, size=0):
 
+        """ initialize new Object
+        Args:
+            size(int): size of new square
+        Raises:
+            TypeError: if type of size is not integer
+            ValueError: if value < 0
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
