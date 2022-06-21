fed0e3c73c3a7b12c1abd2a29d65a9aee136e4d7
Getacher <Getacher.d@gmail.com>
2022-06-22 01:54:15 +0300

    fix zero

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -66,7 +66,7 @@ class Square:
         if self.__size == 0:
             print("")
             return
-        [print("", end="") for i in range(self.__position[1])]
+        [print("") for i in range(self.__position[1])]
         for i in range(self.__size):
             [print(" ", end="") for k in range(self.__position[0])]
             [print("#", end="") for j in range(self.__size)]
@@ -76,7 +76,7 @@ class Square:
 
         """ str representation of object as my print"""
         if self.__size != 0:
-            [print("", end="") for i in range(self.__position[1])]
+            [print("") for i in range(self.__position[1])]
         for i in range(self.__size):
             [print(" ", end="") for k in range(self.__position[0])]
             [print("#", end="") for j in range(self.__size)]
