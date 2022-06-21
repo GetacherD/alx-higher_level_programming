3e718ee340ac85ca247f67af7f80fa4ba2cacf91
Getacher <Getacher.d@gmail.com>
2022-06-21 20:54:30 +0300

    add docs

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+""" Define Class Square. """
 
 
 class Square:
 
+    """ Squre Class."""
     def __init__(self, size):
 
         """ initialize new object Creation
+        Args:
+            size(int): size of new square
         """
         self.__size = size
