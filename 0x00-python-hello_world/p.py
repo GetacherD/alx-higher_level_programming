a21817241643995cdc79593cdc2a323529f61dde
GetacherD <getacher.d@gmail.com>
2022-06-24 13:30:28 -0700

    matrxi final

--- 0x00-python-hello_world/p.py
+++ 0x00-python-hello_world/p.py
@@ -1,13 +1,13 @@
 d = 0
-b = [[2, 3], [6, 7], [8, 9]]
-b = [[b[j][i] for j in range(len(b))] for i in range(len(b[0]))]
-a =[ [1, 2, 1] ,[2 , 1, 2], [2, 2, 2]]
+b = [[3, 4], [5, 6]]
+# b = [[b[j][i] for j in range(len(b))] for i in range(len(b[0]))]
+a = [[1, 4,2]]
 e =[]
 f = []
 for row in range(len(a)):
-    for col in range(len(b)):
-        for x, y in zip(a[row], b[col]):
-            d += x * y
+    for col in range(len(b[0])):
+        for k in range(len(a[row])):
+            d += a[row][k] * b[k][col]
         e.append(d)
         d = 0
     f.append(e)
