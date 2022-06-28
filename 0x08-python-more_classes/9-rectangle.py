e09b9b3ee6156f38a830fe3da2db998e19a8cf21
Getacher <Getacher.d@gmail.com>
2022-06-28 16:00:39 +0300

    first set width in initializing

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -23,8 +23,8 @@ class Rectangle:
             TypeError: if height or width is not type of int
             ValueError: if height ir width < 0
         """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
