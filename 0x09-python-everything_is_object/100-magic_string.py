5876b31e89f7d86dff21728a7ba3bcb148c904bd
Getacher <Getacher.d@gmail.com>
2022-06-28 12:11:23 +0300

    first

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string():
+    s.n = getattr(n, 0) + 1
+    return s*s.n
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+LockedClass doc
+"""
+
+
+class LockedClass:
+
+    """
+    Create LockedClass that has only first_name attr
+    """
+    __slots__ = "first_name"
