bf1a16bf2a209d35af94811e3901b3ea670e6a02
Getacher <Getacher.d@gmail.com>
2022-07-04 16:41:36 +0300

    isinstance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+test for isinstance
+"""
+
+
+def is_kind_of_class(obj, a_class):
+
+    """
+    check instance of object
+    Args:
+        obj: object to be checked
+        a_class: class to be checked against
+    """
+    return isinstance(obj, a_class)
