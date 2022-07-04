7ec34d95cb60cca1f899738b3d99d262fcdd444b
Getacher <Getacher.d@gmail.com>
2022-07-04 17:24:50 +0300

    use format str

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -25,6 +25,6 @@ class BaseGeometry:
             value(int): integer value to be validated
         """
         if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
