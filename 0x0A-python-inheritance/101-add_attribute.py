13a3ac4735fe8f660fcf36495f4dc548e74ef4dc
Getacher <Getacher.d@gmail.com>
2022-07-04 21:09:34 +0300

    first

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+add attributes
+"""
+
+
+def add_attribute(obj, name, value):
+
+    """
+    add attributes
+    Args:
+        obj: to add attributes
+        name: name of attr
+        value: value of attr
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, name)
