dbd0791810538696c3bb41e0a9cac1a899e5522a
Getacher <Getacher.d@gmail.com>
2022-07-04 20:33:01 +0300

    add doc

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,39 +1,29 @@
 #!/usr/bin/python3
 """
-Geometry Base Class module doc
+Integer validator
 """
 
 
 class BaseGeometry:
+    """An empty class"""
 
-    """
-    Blue print for BaseGeometry Objects
-    """
     def area(self):
+        """Raises an exception because...
+        area is not implemented
 
-        """
-        Calculate Area
-
-        Raises:
-            Exception: if not implemented
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-
-        """
-        Validate input parameters
+        """Validates the inputs: name and values
 
         Args:
-            name(str): name of param to be validated
-            value(int): param to be validated
-        Raises:
-            TypeError: if value is not integer
-            ValueError: if value <= 0
-        """
+            name(str) - input name as string
+            value(int): pararams validator
 
-        if not (type(value) == int):
-            raise TypeError(f"{name} must be an integer")
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
         return value
