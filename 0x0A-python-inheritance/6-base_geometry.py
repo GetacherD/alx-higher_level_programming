589d59ce674faaf54152035118d5c6e16d50e04a
Getacher <Getacher.d@gmail.com>
2022-07-04 16:59:16 +0300

    first

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Geometry Base Class
+"""
+
+
+class BaseGeometry:
+
+    """
+    BaseGeometry class
+    """
+    def area(self):
+
+        """
+        Calculate Area
+        """
+        raise Exception("area() is not implemented")
