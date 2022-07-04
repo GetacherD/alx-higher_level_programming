8e5dc820da8b15174a69538c84e41c5baaad65a3
Getacher <Getacher.d@gmail.com>
2022-07-04 17:20:58 +0300

    first

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Geometry Base Class
+"""
+
+
+class BaseGeometry:
+
+    """
+    BaseGeometry class
+    """
+    def area(self):
+
+        """
+        Calculate Area
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+
+        """
+        Validate inputs
+        Args:
+            name(str): name of Geometry
+            value(int): integer value to be validated
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
