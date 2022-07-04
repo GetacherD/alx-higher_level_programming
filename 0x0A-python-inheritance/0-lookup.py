64719dad1ae3614cf4b6ff207f1977160cd74278
Getacher <Getacher.d@gmail.com>
2022-07-04 15:57:03 +0300

    add doc

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,4 +6,11 @@ dir built in module example
 
 def lookup(obj):
 
+    """
+    list all attributes of any object
+    Args:
+        obj(object): any object Type
+    Returns:
+        list of all attributes
+    """
     return list(dir(obj))
