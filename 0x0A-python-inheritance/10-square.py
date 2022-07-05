fcc9c830b353c2feadf83360ea6e60a3e0d5f448
Getacher <Getacher.d@gmail.com>
2022-07-05 07:18:55 +0300

    test

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -17,5 +17,6 @@ class Square(Rectangle):
         Args:
             size(int): size of square
         """
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
