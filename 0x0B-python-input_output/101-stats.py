bc230ca4add52a133d5f92bdd43a7990e9d65c33
Getacher <Getacher.d@gmail.com>
2022-07-05 16:23:54 +0300

    first

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+stats
+"""
+import sys
+
+total = 0
+count = 0
+stats = [200, 301, 400, 401, 403, 404, 405, 500]
+stat_counts = [0] * 8
+try:
+    for line in sys.stdin:
+        total += int(line.split(" ")[-1])
+        count += 1
+        for s in stats:
+            if s == int(line.split(" ")[-2]):
+                stat_counts[stats.index(s)] += 1
+        if count == 10:
+            print("File size: {}".format(total))
+            count = 0
+            for i in range(len(stats)):
+                if stat_counts[i]:
+                    print("{}: {}".format(stats[i], stat_counts[i]))
+except KeyboardInterrupt:
+    print("File size: {}".format(total))
+    for i in range(len(stats)):
+        if stat_counts[i]:
+            print("{}: {}".format(stats[i], stat_counts[i]))
