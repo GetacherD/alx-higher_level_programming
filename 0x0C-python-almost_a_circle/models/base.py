2751594c2b1c78fa20df37b6739b6495c7373a23
GetacherD <getacher.d@gmail.com>
2022-07-11 19:53:24 +0300

    update folder

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,7 +31,7 @@ class Base:
         import json
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        return json.dumps(list_dictionaries)
+        return [json.dumps(d) for d in list_dictionaries]
 
     @classmethod
     def save_to_file(cls, list_objs):
