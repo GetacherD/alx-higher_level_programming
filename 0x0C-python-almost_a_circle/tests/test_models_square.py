a01124ab31aec9ea98b789d9f73e0950a8f16142
GetacherD <getacher.d@gmail.com>
2022-07-12 12:08:54 +0300

    update square test

--- 0x0C-python-almost_a_circle/tests/test_models_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models_square.py
@@ -53,3 +53,155 @@ class TestSquare(unittest.TestCase):
         self.rec.height = 20
         self.rec.width = 10
         self.assertEqual(self.rec.area(), 200)
+
+    def test_sq_1(self):
+
+        """ Test for 1 arg"""
+        s = Square(1)
+        self.assertEqual(s.width, 1)
+        self.assertEqual(s.height, 1)
+
+    def test_sq_2(self):
+
+        """ Test for 2 args """
+        s = Square(2, 4)
+        self.assertEqual(s.width, 2)
+        self.assertEqual(s.height, 2)
+        self.assertEqual(s.x, 4)
+
+    def test_sq_3(self):
+
+        """ Test for 3 args """
+        s = Square(4, 6, 8)
+        self.assertEqual(s.width, 4)
+        self.assertEqual(s.height, 4)
+        self.assertEqual(s.x, 6)
+        self.assertEqual(s.y, 8)
+
+    def test_sq_4(self):
+
+        """ Test for 4 args """
+        s = Square(3, 5, 6, 9)
+        self.assertEqual(s.width, 3)
+        self.assertEqual(s.height, 3)
+        self.assertEqual(s.x, 5)
+        self.assertEqual(s.y, 6)
+        self.assertEqual(s.id, 9)
+
+    def test_str(self):
+
+        """ test __str__ """
+        s = Square(1, 2, 3, 4)
+        self.assertEqual(s.__str__(), "[Square] (4) 2/3 - 1")
+
+    def test_update_1(self):
+
+        """ test for 1 arg """
+        s = Square(1)
+        s.update(34)
+        self.assertEqual(s.id, 34)
+
+    def test_update_2(self):
+
+        """ Test for 2 args """
+        s = Square(1)
+        s.update(45, 55)
+        self.assertEqual(s.id, 45)
+        self.assertEqual(s.width, 55)
+        self.assertEqual(s.height, 55)
+
+    def test_update_3(self):
+
+        """ Test for 3 args """
+        s = Square(1)
+        s.update(3, 4, 5)
+        self.assertEqual(s.id, 3)
+        self.assertEqual(s.width, 4)
+        self.assertEqual(s.height, 4)
+        self.assertEqual(s.x, 5)
+
+    def test_update_4(self):
+
+        """ Test for 4 args """
+        s = Square(1)
+        s.update(33, 44, 55, 66)
+        self.assertEqual(s.id, 33)
+        self.assertEqual(s.width, 44)
+        self.assertEqual(s.height, 44)
+        self.assertEqual(s.x, 55)
+        self.assertEqual(s.y, 66)
+
+    def test_create_1(self):
+
+        """ test for 1 arg """
+        s = Square.create(**{"id": 34})
+        self.assertEqual(s.id, 34)
+
+    def test_create_2(self):
+
+        """ Test for 2 args """
+        s = Square.create(**{"id": 45, "size": 55})
+        self.assertEqual(s.id, 45)
+        self.assertEqual(s.width, 55)
+        self.assertEqual(s.height, 55)
+
+    def test_create_3(self):
+
+        """ Test for 3 args """
+        s = Square.create(**{"id": 3, "size": 4, "x": 5})
+        self.assertEqual(s.id, 3)
+        self.assertEqual(s.width, 4)
+        self.assertEqual(s.height, 4)
+        self.assertEqual(s.x, 5)
+
+    def test_create_4(self):
+
+        """ Test for 4 args """
+        s = Square.create(**{"id": 33, "size": 44, "x": 55, "y": 66})
+        self.assertEqual(s.id, 33)
+        self.assertEqual(s.width, 44)
+        self.assertEqual(s.height, 44)
+        self.assertEqual(s.x, 55)
+        self.assertEqual(s.y, 66)
+
+    def test_type_width(self):
+
+        """ Test for type check of width """
+        with self.assertRaises(TypeError):
+            Square("1")
+
+    def test_type_x(self):
+
+        """ Test for type check -x """
+        with self.assertRaises(TypeError):
+            Square(1, "3")
+
+    def test_type_y(self):
+
+        """ Test type check -y """
+        with self.assertRaises(TypeError):
+            Square(1, 2, "1")
+
+    def test_value_width(self):
+
+        """ Test value check -size """
+        with self.assertRaises(ValueError):
+            Square(-1)
+
+    def test_value_width_0(self):
+
+        """ Test value 0 of width """
+        with self.assertRaises(ValueError):
+            Square(0)
+
+    def test_value_x(self):
+
+        """ Test for value check x """
+        with self.assertRaises(ValueError):
+            Square(1, -1)
+
+    def test_value_y(self):
+
+        """ test value check -y """
+        with self.assertRaises(ValueError):
+            Square(1, 2, -1)
