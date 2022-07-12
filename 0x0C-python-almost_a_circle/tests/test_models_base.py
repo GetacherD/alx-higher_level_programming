2d1b522da9705c4963aa1115d6a7e24a7ca7939a
GetacherD <getacher.d@gmail.com>
2022-07-12 08:32:25 +0300

    update

--- 0x0C-python-almost_a_circle/tests/test_models_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models_base.py
@@ -30,3 +30,33 @@ class TestBase(unittest.TestCase):
         self.assertEqual(self.b4.id, 4)
         self.b5 = Base()
         self.assertEqual(self.b5.id, 3)
+
+    def test_to_json_None(self):
+
+        """ test to_json_string with None """
+        self.assertEqual(Base.to_json_string(None), "[]")
+
+    def test_to_json_emp(self):
+
+        """ Test for [] """
+        self.assertEqual(Base.to_json_string([]), "[]")
+
+    def test_base_to_json_normal(self):
+
+        """ Test for Normal case """
+        self.assertEqual(Base.to_json_string([{"id": 12}]), '[{"id": 12}]')
+
+    def test_from_json_None(self):
+
+        """ test from json None """
+        self.assertEqual(Base.from_json_string(None), [])
+
+    def test_from_json_empty(self):
+
+        """ Test empty """
+        self.assertEqual(Base.from_json_string([]), [])
+
+    def test_from_json_Normal(self):
+
+        """ test for normal """
+        self.assertEqual(Base.from_json_string('[{"id": 12}]'), [{"id": 12}])
