6e80bcd20d5c7c5936ec78ae1b0a47c7e952c2aa
GetacherD <getacher.d@gmail.com>
2022-08-10 13:08:33 +0300

    test

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa; 
 CREATE TABLE IF NOT EXISTS
-cities (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
        state_id INT NOT NULL,
        name VARCHAR(256) NOT NULL,
-       FOREIGN KEY(state_id) REFERENCES states(id)) type=InnoDB;
+       FOREIGN KEY (state_id) REFERENCES states(id),
+       PRIMARY KEY (id)) type=InnoDB;
