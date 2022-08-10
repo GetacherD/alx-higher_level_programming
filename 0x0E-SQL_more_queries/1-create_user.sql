a0915f48a8e9e0f36084bf446354bccd3858c6dc
GetacherD <getacher.d@gmail.com>
2022-08-10 12:15:54 +0300

    fix quote

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- CREATE USER
-create user IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY "user_0d_1_
+create user IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY "user_0d_1_
 pwd";
-GRANT ALL ON *.* TO user_0d_1@localhost;
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
