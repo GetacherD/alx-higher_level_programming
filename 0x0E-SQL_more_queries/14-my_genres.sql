18cdf3d8aaa8024bdf6f1f47f143b65be785adbf
GetacherD <getacher.d@gmail.com>
2022-08-10 15:41:22 +0300

    fix typo

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,7 +2,7 @@
 SELECT g.name FROM
 tv_shows AS s INNER JOIN
 tv_show_genres AS tg
-AS s.id = tg.show_id
+ON s.id = tg.show_id
 INNER JOIN tv_genres AS g
 ON g.id = tg.genre_id
 WHERE s.title = "Dexter" ORDER BY g.name;
