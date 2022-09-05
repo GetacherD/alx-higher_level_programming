f3fcd231308be9a9ab1098ffe8a9aff494ce655e
GetacherD <getacher.d@gmail.com>
2022-09-05 04:52:21 -0700

    fix typo

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const proc = require('process');
-const args = proc.argv;
-console.log(`${args[2] is ${args[3]}`);
+const args = proc.argv;i
+console.log(`${args[2]} is ${args[3]}`);
