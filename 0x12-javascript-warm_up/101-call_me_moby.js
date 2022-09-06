ac692382b82a94d64d16ce44932638888bb1c189
GetacherD <getacher.d@gmail.com>
2022-09-06 20:15:26 +0300

    fix typo

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.callMe = function (x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
   let i = 0;
   while (i < Math.floor(Number(x))) {
     theFunction();
