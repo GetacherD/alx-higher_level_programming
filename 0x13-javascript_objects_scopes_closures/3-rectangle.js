5451af4e503334226701d072e04fe1ec35ea8162
GetacherD <getacher.d@gmail.com>
2022-09-06 15:30:49 +0300

    first

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || w === undefined) {
+      return this;
+    } else if (h <= 0 || h === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    while (i < Math.floor(Number(this.height))) {
+      console.log('x'.repeat(Math.floor(Number(this.width))));
+      i++;
+    }
+  }
+};
