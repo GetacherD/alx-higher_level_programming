174dee14e06b287e3f1a761ece2b4859c0a7e5d4
GetacherD <getacher.d@gmail.com>
2022-09-06 03:30:19 +0300

    compare NaN

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const proc = require('process');
 const num = Number(proc.argv[2]);
-if (num != num || num === undefined) {
+const num2 = Number(proc.argv[2]);
+if (num2 !== num || num === undefined) {
   console.log('Not a number');
 } else {
   console.log(`My number: ${num}`);
