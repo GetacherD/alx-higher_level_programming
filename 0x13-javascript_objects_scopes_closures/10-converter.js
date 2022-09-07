7f7f9181ff344bfed0cfaeca17d0da953bf107d1
GetacherD <getacher.d@gmail.com>
2022-09-07 15:17:45 +0300

    add explanation

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  return num => num.toString(base);
+// Return a funciton that takes a number and convert to base specified in parent function
+  return (num) => num.toString(base);
 };
