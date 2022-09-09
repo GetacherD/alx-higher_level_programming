2d1e81cd53710baea1abdc8f07ea5b2675cfe342
GetacherD <getacher.d@gmail.com>
2022-09-09 11:01:30 +0300

    first

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script select states name matching argumnet given
+"""
+import sys
+import MySQLdb as sdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    conn = sdb.connect(host="localhost",
+                       user=username,
+                       passwd=password,
+                       db=dbname,
+                       port=3306)
+    cur = conn.cursor()
+    query = """SELECT * FROM states where BINARY name=%s
+            ORDER BY id ASC"""
+    name = (sys.argv[4],)
+    cur.execute(query, name)
+    result = cur.fetchall()
+    for r in result:
+        print(r)
