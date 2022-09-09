877de8f59aa194822044da48873631d62da1cc97
GetacherD <getacher.d@gmail.com>
2022-09-09 09:55:18 +0300

    first

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script to select names start with n or N
+"""
+import sys
+import MySQLdb as sdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    conn = sdb.connect(host="localhost",
+                       user=username,
+                       passwd=password,
+                       db=dbname,
+                       port=3306)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states where name like 'N%'")
+    result = cur.fetchall()
+    for r in result:
+        print(r)
