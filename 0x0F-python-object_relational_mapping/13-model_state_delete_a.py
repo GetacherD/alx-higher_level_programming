a3b146f5098948853422d6885f8d3aef9510e086
GetacherD <getacher.d@gmail.com>
2022-09-09 14:16:00 +0300

    first

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script first state object
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            username, password, dbname))
+    session = sessionmaker(bind=engine)()
+    session.query(State).filter(
+        State.name.contains("a")).delete(
+            synchronize_session=False)
+    session.commit()
