5b67a83bfda664fcf925e372c08bb9f261a4aab5
GetacherD <getacher.d@gmail.com>
2022-09-09 09:43:14 +0300

    first

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+MySQLdb demo
+"""
+
+import sys
+import MySQLdb as sdb
+
+
+username = sys.argv[1]
+password = sys.argv[2]
+dbname = sys.argv[3]
+conn = sdb.connect(host="localhost",
+                   user=username, passwd=password,
+                   db=dbname, port=3306)
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+result = cur.fetchall()
+for r in result:
+    print(r)
