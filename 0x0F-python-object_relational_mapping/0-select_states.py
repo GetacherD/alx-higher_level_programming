dc8f20825566330ce12fa06ef89bbd2cf35fe592
GetacherD <getacher.d@gmail.com>
2022-09-09 09:47:12 +0300

    prevent script run when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,15 +6,15 @@ MySQLdb demo
 import sys
 import MySQLdb as sdb
 
-
-username = sys.argv[1]
-password = sys.argv[2]
-dbname = sys.argv[3]
-conn = sdb.connect(host="localhost",
-                   user=username, passwd=password,
-                   db=dbname, port=3306)
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-result = cur.fetchall()
-for r in result:
-    print(r)
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    conn = sdb.connect(host="localhost",
+                       user=username, passwd=password,
+                       db=dbname, port=3306)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    result = cur.fetchall()
+    for r in result:
+        print(r)
