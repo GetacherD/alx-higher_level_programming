fcfa791f549fe04724ae7c6aa1751306ee261ab2
GetacherD <getacher.d@gmail.com>
2022-09-09 10:21:46 +0300

    first

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script select states name matching argumnet given
+"""
+import sys
+import MySQLdb as sdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    name = sys.argv[4]
+    conn = sdb.connect(host="localhost",
+                       user=username,
+                       passwd=password,
+                       db=dbname,
+                       port=3306)
+    cur = conn.cursor()
+    query = "SELECT * FROM states where name='{}' ORDER BY id ASC".format(name)
+    cur.execute(query)
+    result = cur.fetchall()
+    for r in result:
+        print(r)
