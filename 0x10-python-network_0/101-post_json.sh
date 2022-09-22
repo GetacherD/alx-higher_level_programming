5a23d9be969a5df3e569ecb4dbf62d4629214329
GetacherD <getacher.d@gmail.com>
2022-09-22 09:05:55 -0700

    first

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # post json data
-curl "$1" -sX POST -H 'Content-Type: application/json' -d '{"name": "John Doe","age": 33}'
+curl "$1" -sX POST -H 'Content-Type: application/json' -d "@$2"
