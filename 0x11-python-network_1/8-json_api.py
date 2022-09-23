5ac3efc145749a3c9650299abba6ac9cce959dc3
GetacherD <getacher.d@gmail.com>
2022-09-23 07:27:52 -0700

    first

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+search with get
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    payload = {'q': len(sys.argv) >= 3 ? sys.argv[2]: ""}
+    resp = requests.get(sys.argv[1], params=payload)
+    try:
+        data = resp.json()
+        if data:
+            for key, value in (dict(data)).items():
+                print("[{}] {}".format(key, value))
+        else:
+            print("No result")
+    except BaseException:
+        print("Not a valid JSON")
