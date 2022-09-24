27dcfabd1f38baca83c77f6fc4c1ab056801f8bd
GetacherD <getacher.d@gmail.com>
2022-09-24 03:17:58 +0300

    select top 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/"+str(
         sys.argv[2]) + "/" + str(sys.argv[1]) + "/commits"
     resp = requests.get(url)
-    data = list(resp.json())
+    data = list(resp.json())[0:10]
     try:
         for item in data:
             print("{}: {}".format(item.get(
