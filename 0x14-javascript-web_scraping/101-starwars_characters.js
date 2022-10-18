14e17c1d2c69fb1cd2e4c99aa97604e63f7fdb2f
GetacherD <getacher.d@gmail.com>
2022-10-18 20:39:19 +0300

    pass exception

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,12 +5,14 @@ const loadData = async () => {
     const res = await req.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2]);
     const data = await JSON.parse(res).characters;
     for (const buddy of data) {
-      const man = await req.get(buddy);
-      const name = await JSON.parse(man).name;
-      console.log(name);
+      try {
+        const man = await req.get(buddy);
+        const name = await JSON.parse(man).name;
+        console.log(name);
+      } catch (e) {
+      }
     }
   } catch (e) {
-    console.log(e);
   }
 };
 loadData();
