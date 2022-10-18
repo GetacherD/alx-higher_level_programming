e2a70a16e5df7f5aca76d27f3f48764558d42ea0
GetacherD <getacher.d@gmail.com>
2022-10-18 21:03:14 +0300

    wrapper for request

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,23 +1,28 @@
 #!/usr/bin/node
-const req = require('request');
-req.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, resp, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    const data = JSON.parse(body).characters;
-    print(data, 0);
-  }
-});
-function print (data, index) {
-  req.get(data[index], function (err, resp, body) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(JSON.parse(body).name);
-    }
+
+const request = require('request');
+function doRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, res, body) {
+      if (!error && res.statusCode === 200) {
+        resolve(JSON.parse(body));
+      } else {
+        reject(error);
+      }
+    });
   });
-  const len = data.length;
-  if (index + 1 < len) {
-    print(data, index + 1);
+}
+
+async function main () {
+  try {
+    const response = await doRequest('https://swapi-api.hbtn.io/api/films/' + process.argv[2]);
+    for (const ur of response.characters) {
+      const resp = await doRequest(ur);
+      console.log(resp.name);
+    } // `response` will be whatever you passed to `resolve()` at the top
+  } catch (error) {
+    console.error(error); // `error` will be whatever you passed to `reject()` at the top
   }
 }
+
+main();
