56beeef2d1c669aeb8290f8b4e30018ae15c3277
GetacherD <getacher.d@gmail.com>
2022-10-18 18:37:08 +0300

    test

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,22 +6,14 @@ req.get(process.argv[2], { json: true }, function (err, resp, body) {
     console.error(err);
   } else {
     const res = {};
+
     for (const todo of body) {
-      if (res[todo.userId] === undefined) {
-        res[todo.userId] = 0;
-      } else {
-        if (todo.completed === true) {
-          res[todo.userId] = res[todo.userId] + 1;
-        }
+      if (res[todo.userId] === undefined && todo.completed === true) {
+        res[todo.userId] = 1;
+      } else if (todo.completed === true) {
+        res[todo.userId] += 1;
       }
     }
-    let out = '{';
-    for (const key in res) {
-      out += "  '" + key + "': " + res[key] + ',\n';
-    }
-    out = out.slice(2, -2);
-    out = '{' + out;
-    out += ' }';
-    console.log(out);
+    console.log(res);
   }
 });
