a15739f370e087ca3dd4762be70ed2e16377dc37
GetacherD <getacher.d@gmail.com>
2022-10-18 16:36:27 +0300

    fix out put text

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ req.get(process.argv[2], function (err, resp, body) {
   if (err) {
     console.error(err);
   } else {
-    console.log("code:", resp.statusCode);
+    console.log('code:', resp.statusCode);
   }
 });
